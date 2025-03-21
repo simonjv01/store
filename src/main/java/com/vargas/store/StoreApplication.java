fb8e76c633ad62d3b167bc9f00970f04ca171f3e
Simon Vargas <sjvargas01@gmail.com>
2025-03-20 23:00:19 -0400

    added spring application context

--- src/main/java/com/vargas/store/StoreApplication.java
+++ src/main/java/com/vargas/store/StoreApplication.java
@@ -2,14 +2,19 @@ package com.vargas.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.apple.eawt.Application;
 
 @SpringBootApplication
 public class StoreApplication {
 
     public static void main(String[] args) {
 
-//        SpringApplication.run(StoreApplication.class, args);
-        var orderService = new OrderService(new PayPalPaymentService());
+       ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
+       var orderService = context.getBean(OrderService.class);
+
+        //var orderService = new OrderService(new PayPalPaymentService());
         
         orderService.placeOrder();
     }
