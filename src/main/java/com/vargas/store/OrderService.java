c5f7c589da2e8c580f59eaf8fac63db06e9c5989
Simon Vargas <sjvargas01@gmail.com>
2025-03-20 23:09:06 -0400

    added autowired annotation example

--- src/main/java/com/vargas/store/OrderService.java
+++ src/main/java/com/vargas/store/OrderService.java
@@ -1,12 +1,20 @@
 package com.vargas.store;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+
+@Service
 public class OrderService {
 
 
     private PaymentService paymentService;
 
+    public OrderService() {
+    }
+    
+    @Autowired
     public OrderService(PaymentService paymentService) {
         this.paymentService = paymentService;
     }
--- src/main/java/com/vargas/store/PayPalPaymentService.java
+++ src/main/java/com/vargas/store/PayPalPaymentService.java
@@ -1,5 +1,8 @@
 package com.vargas.store;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class PayPalPaymentService implements PaymentService {
 
     @Override
